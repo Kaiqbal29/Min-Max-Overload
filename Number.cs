851769c2d94435c03b82f66b402312e93808e7cc
Kaiqbal29 <iqbalfaris@students.amikom.ac.id>
2023-04-11 00:01:29 +0700

    commit

---
+++ Number.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prak5
+{
+    public class Number
+    {
+       
+            public int Min(int a, int b, int c)
+            {
+                int min = 0;
+                if (a < b && a < c)
+                    Console.WriteLine(" Min dari ketiga nilai adalah : {0}", min = a);
+                else if (b < a && b < c)
+                    Console.WriteLine(" Min dari ketiga nilai adalah : {0}", min = b);
+                else if (c < a && c < b)
+                    Console.WriteLine(" Min dari ketiga nilai adalah : {0}", min = c);
+
+                return min;
+            }
+            public int Max(int a, int b, int c)
+            {
+                int max = 0;
+                if (a > b && a > c)
+                    Console.WriteLine(" Max dari ketiga nilai adalah : {0}", max = a);
+                else if (b > a && b > c)
+                    Console.WriteLine(" Max dari ketiga nilai adalah : {0}", max = b);
+                else if (c > a && c > b)
+                    Console.WriteLine(" Max dari ketiga nilai adalah : {0}", max = c);
+
+                return max;
+            }
+        
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using prak5;
+using System;
+namespace prak5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.Write(" Masukan Nilai A : ");
+            a = int.Parse(Console.ReadLine() );
+            Console.Write(" Masukan Nilai B : ");
+            b = int.Parse(Console.ReadLine() );
+            Console.Write(" Masukan Nilai C : " );
+            c = int.Parse(Console.ReadLine());
+
+            Console.ReadLine();
+            Number Nilai = new Number();
+            Nilai.Min(a, b, c);
+            Nilai.Max(a, b, c);
+            Console.ReadKey();
+        }
+    }
+}
